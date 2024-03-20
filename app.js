45814bb644cf09e840b9d17c98263f22f7e8cc37
Aryankr484 <93274098+Aryankr484@users.noreply.github.com>
2024-03-20 10:06:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express=require('express');
+const expresslayout=require('express-ejs-layouts');
+
+const connectDB=require('./server/config/db');
+
+const app=express();
+const PORT=3000||process.env.PORT;
+
+connectDB();
+
+app.use(express.static('public'));
+
+app.use(expresslayout);
+app.set('layout','./layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'));
+app.get('', (req,res)=>{
+    res.send("Hello world");
+})
+app.listen(PORT, ()=>{
+    console.log(`App listening on port ${PORT}`);
+});
---
+++ server/routes/main.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router=express.Router();
+
+router.get('',(req,res)=>{
+    const locals={
+        title:"Nodejs Blog",
+        description:"Simple Blog created with Nodejs, Express & Mongodb"
+    }
+    res.render('index',{locals});
+});
+router.get('/about',(req,res)=>{
+    res.render('about');
+});
+
+module.exports=router;
